3b0516f32d210efed56113e635c3457e79925017
BeanCan0325 <bean.can0325@gmail.com>
2026-02-09 14:52:55 +0900

    Title: 반복 출력하기, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260209/반복 출력하기/repeated-output.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        mdd(n);
+        }
+     static void mdd (int a){
+            for(int i =0 ; i<a; i++){
+            System.out.println("12345^&*()_");
+            }  // Please write your code here.
+    }
+    
+}
