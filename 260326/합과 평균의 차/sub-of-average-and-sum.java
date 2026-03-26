bf77e85c197c4b74a8655497479eb80f77ba9aac
BeanCan0325 <bean.can0325@gmail.com>
2026-03-26 21:38:36 +0900

    Title: 합과 평균의 차, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260326/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+
+        int D = a+b+c;
+        int E = E/3;
+        
+        System.out.println(D);
+        System.out.println(E);
+
+        System.out.printls(D-E);
+
+
+    }
+}
